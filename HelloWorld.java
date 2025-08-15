9d802f238a81c1378c89eac8d71183052f93b7b8
Katie Tracy <ktracy2@emich.edu>
2025-08-15 14:02:18 -0400

    feat: personalize greeting

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,8 @@
 public class HelloWorld {
     public static void main(String[] args) {
         // TODO: Replace the placeholders with your details.
-        String studentName = "YOUR_NAME";
-        String funFact = "YOUR_FUN_FACT (e.g., I love birding!)";
+        String studentName = "KATIE TRACY";
+        String funFact = "I have a pet cockatiel named Breeki!";
         System.out.println("Hello from " + studentName + "!");
         System.out.println("Fun fact: " + funFact);
     }
