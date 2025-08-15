882a519ac550bf02b6b41bde4aef8da5ec210001
ktracy2 <78753017+ktracy2@users.noreply.github.com>
2025-08-15 13:18:37 -0400

    Add file via upload, initial HelloWorld with TODOs

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        // TODO: Replace the placeholders with your details.
+        String studentName = "YOUR_NAME";
+        String funFact = "YOUR_FUN_FACT (e.g., I love birding!)";
+        System.out.println("Hello from " + studentName + "!");
+        System.out.println("Fun fact: " + funFact);
+    }
+}
